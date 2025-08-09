c760e4c7f3a44cd16b509704def934ba13c13bcf
PaulaMaged <128493887+PaulaMaged@users.noreply.github.com>
2025-08-09 05:58:36 +0300

    Provide data from logic to UI through functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,257 +1,330 @@
 const gameBoard = (function () {
-  const generateBoard = (emptyToken) => {
-    let board = [];
-    for (let i = 0; i < 3; i++) {
-      board[i] = [];
-      for (let j = 0; j < 3; j++) {
-        board[i].push(Cell(emptyToken, i, j));
-      }
-    }
-
-    return board;
-  };
-
-  const printBoard = (board) => {
-    let stringOutput = "";
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (j == 2) {
-          stringOutput += board[i][j].getToken();
-        } else {
-          stringOutput += `${board[i][j].getToken()}|`;
+    const generateBoard = (_emptyToken) => {
+        let _board = [];
+        for (let i = 0; i < 3; i++) {
+            _board[i] = [];
+            for (let j = 0; j < 3; j++) {
+                _board[i].push(Cell(_emptyToken, i, j));
+            }
         }
 
-        if (j == 2 && i != 2) {
-          stringOutput += "\n------\n";
+        return _board;
+    };
+
+    const printBoard = (_board) => {
+        let stringOutput = '';
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const char = _board[i][j] == '' ? ' ' : _board[i][j];
+                if (j == 2) {
+                    stringOutput += `\t${char}`;
+                } else {
+                    stringOutput += `\t${char}\t|`;
+                }
+
+                if (j == 2 && i != 2) {
+                    stringOutput += '\n--------------------------\n';
+                }
+            }
         }
-      }
-    }
 
-    console.log(stringOutput);
-  };
+        console.log(stringOutput);
+    };
 
-  return { generateBoard, printBoard };
+    return { generateBoard, printBoard };
 })();
 
-function Cell(emptyToken, i, j) {
-  let token = emptyToken;
-  const position = { row: i, column: j };
+function Cell(_emptyToken, i, j) {
+    let token = _emptyToken;
+    const position = { row: i, column: j };
 
-  const getToken = () => token;
-  const setToken = (value) => (token = value);
-  const getPosition = () => position;
-  return {
-    setToken,
-    getToken,
-    getPosition,
-  };
+    const getToken = () => token;
+    const setToken = (value) => (token = value);
+    const getPosition = () => position;
+    return {
+        setToken,
+        getToken,
+        getPosition,
+    };
 }
 
-const gameController = (function (
-  playerOneName = "Player One",
-  playerOneToken = "X",
-  playerTwoName = "Player Two",
-  playerTwoToken = "O",
-  emptyToken = ""
-) {
-  const players = [
-    {
-      name: playerOneName,
-      token: playerOneToken,
-      wins: 0,
-    },
-    {
-      name: playerTwoName,
-      token: playerTwoToken,
-      wins: 0,
-    },
-  ];
+const gameController = (function () {
+    const _emptyToken = '';
+    let _players = null;
+    let _activePlayer = null;
+    let _lastTurnStats = {
+        position: [],
+        player: null,
+    };
 
-  let isGameRunning = false;
-  let startingPlayer = players[0];
-  let activePlayer = players[0];
-  let board;
+    let _turn = null;
 
-  const startNewGame = () => {
-    isGameRunning = true;
-    board = gameBoard.generateBoard(emptyToken);
+    let _gameStatus = {
+        isGameOver: null,
+        gameOverReason: null,
+        winningConfiguration: null,
+    };
 
-    //reset wins
-    for (let player of players) {
-      player.wins = 0;
+    let _board;
+
+    const _winningConfigurations = (() => {
+        //re-implement winning check
+        const _winningConfigurations = [];
+
+        //horizontal winning configurations
+        for (let i = 0; i < 3; i++) {
+            _winningConfigurations.push([]);
+            for (let j = 0; j < 3; j++) {
+                _winningConfigurations[i].push([i, j]);
+            }
+        }
+
+        //vertical winning configurations
+        for (let i = 3; i < 6; i++) {
+            _winningConfigurations.push([]);
+            for (let j = 0; j < 3; j++) {
+                const columnIndex = i - 3;
+                _winningConfigurations[i].push([j, columnIndex]);
+            }
+        }
+
+        //diagonal winning configurations
+        _winningConfigurations.push([
+            [0, 0],
+            [1, 1],
+            [2, 2],
+        ]);
+
+        _winningConfigurations.push([
+            [2, 0],
+            [1, 1],
+            [0, 2],
+        ]);
+
+        return _winningConfigurations;
+    })();
+
+    const startNewGame = (
+        players = [
+            { name: 'Player One', token: 'X' },
+            { name: 'Player Two', token: 'O' },
+        ],
+        startingPlayer = players[0],
+    ) => {
+        _board = gameBoard.generateBoard(_emptyToken);
+
+        _players = players;
+        _activePlayer = startingPlayer;
+        _lastTurnStats = {
+            position: [],
+            player: null,
+        };
+
+        _gameStatus = {
+            isGameOver: false,
+            gameOverReason: null,
+            winningConfiguration: null,
+        };
+
+        _turn = 1;
+    };
+
+    const getBoard = () => {
+        let boardCopy = [];
+        for (let i = 0; i < 3; i++) {
+            boardCopy[i] = [];
+            for (let j = 0; j < 3; j++) {
+                boardCopy[i].push(_board[i][j].getToken());
+            }
+        }
+        return boardCopy;
+    };
+
+    const playTurn = (i, j) => {
+        const turnStatus = {
+            isValid: null,
+            errorMessage: '',
+        };
+
+        if (!isValidMove(i, j)) {
+            turnStatus.errorMessage =
+                'Action is invalid, please choose an empty space';
+            turnStatus.isValid = false;
+            return turnStatus;
+        }
+
+        _board[i][j].setToken(_activePlayer.token);
+
+        _lastTurnStats.position = [i, j];
+        _lastTurnStats.player = _activePlayer.token;
+        _turn++;
+
+        updateGameStatus();
+        if (_gameStatus.isGameOver) {
+            endGame();
+            turnStatus.isValid = true;
+            return turnStatus;
+        }
+
+        _activePlayer =
+            _activePlayer === _players[0] ? _players[1] : _players[0];
+        console.log(`${_activePlayer.name}'s _Turn`);
+
+        turnStatus.isValid = true;
+        return turnStatus;
+    };
+
+    const isValidMove = (i, j) => {
+        console.log({ i, j });
+        if (_board[i][j].getToken() != _emptyToken) {
+            console.log('Invalid move, cell is occupied!');
+            return false;
+        } else if (i < 0 || i > 2 || j < 0 || j > 2) {
+            console.log('Move is outside _board boundaries');
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    const updateGameStatus = () => {
+        for (let configuration of _winningConfigurations) {
+            const cell1 =
+                _board[configuration[0][0]][configuration[0][1]].getToken();
+            const cell2 =
+                _board[configuration[1][0]][configuration[1][1]].getToken();
+            const cell3 =
+                _board[configuration[2][0]][configuration[2][1]].getToken();
+
+            if (cell1 != _emptyToken && cell1 == cell2 && cell1 == cell3) {
+                _gameStatus.isGameOver = true;
+                _gameStatus.gameOverReason = _activePlayer.token;
+                _gameStatus.winningConfiguration = configuration;
+            }
+        }
+
+        if (_gameStatus.isGameOver == false && _turn == 9) {
+            _gameStatus.isGameOver = true;
+            _gameStatus.gameOverReason = 2;
+        }
+    };
+
+    const endGame = () => {
+        console.log(`${_activePlayer.name} Won!`);
+        gameBoard.printBoard(_board);
+        console.log(`${_activePlayer.name}:`);
+    };
+
+    function getGameStatus() {
+        return _gameStatus;
     }
 
-    activePlayer = players[0];
-  };
-
-  const getBoard = () => {
-    let boardRep = [];
-    for (let i = 0; i < 3; i++) {
-      boardRep[i] = [];
-      for (let j = 0; j < 3; j++) {
-        boardRep[i].push(board[i][j].getToken());
-      }
-    }
-  };
-
-  const playRound = (i, j) => {
-    if (!isValidMove(i, j)) {
-      return;
+    function get_lastTurnStats() {
+        return _lastTurnStats;
     }
 
-    board[i][j].setToken(activePlayer.token);
-    if (isGameEnd(board[i][j])) {
-      endGame();
-      return;
-    }
-
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    console.log(`${activePlayer.name}'s Turn`);
-  };
-
-  const isValidMove = (i, j) => {
-    console.log({ i, j });
-    if (board[i][j].getToken() != emptyToken) {
-      console.log("Invalid move, cell is occupied!");
-      return false;
-    } else if (i < 0 || i > 2 || j < 0 || j > 2) {
-      console.log("Move is outside board boundaries");
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  const isGameEnd = (lastCellPlayed) => {
-    const currentToken = lastCellPlayed.getToken();
-
-    //check horizontal
-    const row = lastCellPlayed.getPosition().row;
-    let isHorizontalStreak = true;
-
-    for (let i = 0; i < 3; i++) {
-      if (board[row][i].getToken() != currentToken) {
-        isHorizontalStreak = false;
-        break;
-      }
-    }
-
-    if (isHorizontalStreak) return true;
-
-    //check for vertical
-    const column = lastCellPlayed.getPosition().column;
-    let isVerticalStreak = true;
-
-    for (let i = 0; i < 3; i++) {
-      if (board[i][column].getToken() != currentToken) {
-        isVerticalStreak = false;
-        break;
-      }
-    }
-
-    if (isVerticalStreak) return true;
-
-    //two diagnoals to check
-
-    //first the top left to bottom right diagonal
-    let isTopLeftToBottomRightDiagonalStreak = true;
-    for (let i = 0; i < 3; i++) {
-      if (board[i][i].getToken() != currentToken) {
-        isTopLeftToBottomRightDiagonalStreak = false;
-        break;
-      }
-    }
-
-    if (isTopLeftToBottomRightDiagonalStreak) return true;
-
-    //second the bottom left to top right diagonal
-    let isBottomLeftToTopRightDiagonalStreak = true;
-    for (let i = 2; i >= 0; i--) {
-      let column = 2 - i;
-      if (board[i][column].getToken() != currentToken) {
-        isBottomLeftToTopRightDiagonalStreak = false;
-        break;
-      }
-    }
-
-    if (isBottomLeftToTopRightDiagonalStreak) return true;
-
-    return false;
-  };
-
-  const endGame = () => {
-    console.log(`${activePlayer.name} Won!`);
-    gameBoard.printBoard(board);
-    console.log(`${players[0].name}:`);
-  };
-
-  const restartGame = () => {
-    activePlayer = startingPlayer == players[0] ? players[1] : players[0];
-    board = board.generateBoard();
-  };
-
-  const getStatus = () => {
-    return [Object.assign({}, players[0]), Object.assign({}, players[1])];
-  };
-
-  return {
-    startNewGame,
-    playRound,
-    getStatus,
-    getBoard,
-    restartGame,
-  };
+    return {
+        startNewGame,
+        playTurn,
+        getBoard,
+        get_lastTurnStats,
+        getGameStatus,
+    };
 })();
 
 const screenController = ((gameController) => {
-  //Cache DOM elements
-  const elBoard = document.querySelector(".board");
+    //Cache DOM elements
+    const elBoard = document.querySelector('.board');
+    const cells = [];
+    const players = [
+        {
+            name: 'Player One',
+            wins: 0,
+        },
+        {
+            name: 'Player Two',
+            wins: 0,
+        },
+    ];
 
-  //create necessary layout for displaying information
-  const init = () => {
-    gameController.startNewGame();
-    //create and add elements to status
-    for (let i = 0; i < 9; i++) {
-      const gameCell = document.createElement("div");
-      gameCell.textContent = "X";
-      gameCell.classList.add("cell");
-      const gameCellContainer = document.createElement("div");
-      gameCellContainer.addEventListener("click", handleOnClick);
-      gameCellContainer.dataset.position = i.toString();
-      gameCellContainer.classList.add("cell-container");
-      gameCellContainer.appendChild(gameCell);
-      elBoard.appendChild(gameCellContainer);
+    //create necessary layout for displaying information
+    const init = (
+        playerOneName = 'Player One',
+        playerTwoName = 'Player Two',
+    ) => {
+        gameController.startNewGame();
+        //create and add elements to status
+        for (let i = 0; i < 9; i++) {
+            const gameCell = document.createElement('div');
+            cells.push(gameCell);
+            gameCell.textContent = '';
+            gameCell.classList.add('cell');
+            const gameCellContainer = document.createElement('div');
+            gameCellContainer.addEventListener('click', handleOnClick);
+            gameCellContainer.dataset.position = i.toString();
+            gameCellContainer.classList.add('cell-container');
+            gameCellContainer.appendChild(gameCell);
+            elBoard.appendChild(gameCellContainer);
+        }
+
+        players[0].name = playerOneName;
+        players[1].name = playerTwoName;
+    };
+
+    //render board and status (active player and win count)
+    const render = () => {
+        //render last play
+        const _gameStatus = gameController.getGameStatus();
+        const _lastTurnStats = gameController.get_lastTurnStats();
+        const row = 0;
+        const column = 1;
+        const cellIndex =
+            _lastTurnStats.position[row] * 3 + _lastTurnStats.position[column];
+
+        cells[cellIndex].textContent = _lastTurnStats.player;
+
+        //Allows rendering last turn before ending game.
+        setTimeout(() => {
+            if (_gameStatus.isGameOver) {
+                alert('Game Over');
+                setupNewRound();
+            }
+        }, 0);
+    };
+
+    function random(number) {
+        return Math.floor(Math.random() * (number + 1));
     }
-  };
 
-  //render board and status (active player and win count)
-  const render = () => {};
+    //register events from clicking cells on game _board
+    function handleOnClick(e) {
+        const ordinalPosition = e.currentTarget.dataset.position;
+        const row = Math.trunc(ordinalPosition / 3);
+        const column = ordinalPosition % 3;
+        console.log({ ordinalPosition, row, column });
+        const turnStatus = gameController.playTurn(row, column);
 
-  function random(number) {
-    return Math.floor(Math.random() * (number + 1));
-  }
+        if (turnStatus.isValid) render();
+        else alert(turnStatus.errorMessage);
+    }
 
-  //register events from clicking cells on game board
-  function handleOnClick(e) {
-    const ordinalPosition = e.currentTarget.dataset.position;
-    const row = Math.trunc(ordinalPosition / 3);
-    const column = ordinalPosition % 3;
-    gameController.playRound(row, column);
-    console.log({ ordinalPosition, row, column });
-  }
+    function setupNewRound() {
+        for (let cell of cells) {
+            cell.textContent = '';
+        }
 
-  //invoke restart game method
-  const restartGame = () => {};
+        gameController.startNewGame();
+    }
 
-  //invoke startNewGame method
-  const startNewGame = () => {};
+    const restartGame = () => {
+        gameController.startNewGame();
+    };
 
-  return {
-    init,
-    render,
-    restartGame,
-    startNewGame,
-  };
+    return {
+        init,
+        setupNewRound,
+        restartGame,
+    };
 })(gameController);
 
 screenController.init();
