e622bf0604dc549a168949e65bdc5c5b9a4de06a
PaulaMaged <128493887+PaulaMaged@users.noreply.github.com>
2025-08-12 05:43:46 +0300

    Use CSS normalize, create fluid typography, handle top-level page-container using flex, and add plenty of useful utilities

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -234,7 +234,7 @@ const gameController = (function () {
 
 const screenController = ((gameController) => {
     //Cache DOM elements
-    const elBoard = document.querySelector('.board');
+    const elBoard = document.querySelector('.grid-game-board');
     const cells = [];
     const players = [
         {
@@ -328,3 +328,50 @@ const screenController = ((gameController) => {
 })(gameController);
 
 screenController.init();
+
+//I want to have re-usable html snippets instead of repeating them by hand,
+//they should exist during static run of page, which means hard-coding all that repeating syntax,
+//and making sure that it is consistent with all other repeated parts.s
+
+(() => {
+    const playerCardsContainer = document.querySelector(
+        '#players-stats-container',
+    );
+
+    for (let i = 1; i < 3; i++) {
+        const cardHTML =
+            /* HTML */
+            `
+                <section
+                    class="player-stats-card flex-column gap-sm justify-start align-items-center border-radius-lg padding-horizontal-lg padding-vertical-md"
+                >
+                    <p class="player-stats-card__name">
+                        Player ${i == 1 ? 'One' : 'Two'}
+                        <svg
+                            class="player-stats-card__edit-icon"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <g id="Edit / Edit_Pencil_01">
+                                <path
+                                    id="Vector"
+                                    d="M12 8.00012L4 16.0001V20.0001L8 20.0001L16 12.0001M12 8.00012L14.8686 5.13146L14.8704 5.12976C15.2652 4.73488 15.463 4.53709 15.691 4.46301C15.8919 4.39775 16.1082 4.39775 16.3091 4.46301C16.5369 4.53704 16.7345 4.7346 17.1288 5.12892L18.8686 6.86872C19.2646 7.26474 19.4627 7.46284 19.5369 7.69117C19.6022 7.89201 19.6021 8.10835 19.5369 8.3092C19.4628 8.53736 19.265 8.73516 18.8695 9.13061L18.8686 9.13146L16 12.0001M12 8.00012L16 12.0001"
+                                    stroke="#000000"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </g>
+                        </svg>
+                    </p>
+                    <p class="player-stats-card__player-marker">
+                        ${i == 1 ? 'X' : 'O'}
+                    </p>
+                    <p class="win-score">Wins: 0</p>
+                </section>
+            `;
+
+        playerCardsContainer.innerHTML += cardHTML;
+    }
+})();
